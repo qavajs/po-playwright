d51399aab2e83638ffe4eb60b51cf50bdfd35482
Oleksandr_Halichenko <oleksandr_halichenko@epam.com>
2023-01-23 13:35:39 +0300

    implemented ignoreHierarchy option

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,6 @@
 import { Locator } from 'playwright';
-declare function $(selector: string|Object): Object;
-declare function $$(selector: string|Object): Object;
+declare function $(selector: string|Object, options?: { ignoreHierarchy: boolean }): Object;
+declare function $$(selector: string|Object, options?: { ignoreHierarchy: boolean }): Object;
 declare type PageObject = {
     init(driver, options: { timeout: number }): void;
     register(pageObject: Object): void;
